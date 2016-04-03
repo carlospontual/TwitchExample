943a110b09eb809179008a23e9be1c10b767bada
Carlos Eduardo Pontual <carloscode@gmail.com>
2016-04-03 17:47:44 -0300

    Using Picasso with OkHttp in order to enable download of redirect images

--- app/src/main/java/com/carlospontual/twitch/list/ui/GameListAdapter.java
+++ app/src/main/java/com/carlospontual/twitch/list/ui/GameListAdapter.java
@@ -8,9 +8,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.carlospontual.twitch.list.BuildConfig;
 import com.carlospontual.twitch.list.R;
 import com.carlospontual.twitch.list.data.models.Game;
 import com.carlospontual.twitch.list.data.models.GameData;
+import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -27,10 +29,17 @@ public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.GameVi
     Context context;
     Picasso picasso;
 
+    public GameListAdapter(Context context) {
+        this(context, null);
+    }
+
     public GameListAdapter(Context context, List<Game> games) {
         this.context = context;
         this.games = games;
-        picasso = picasso.with(context);
+        picasso = new Picasso.Builder(context).downloader(new OkHttp3Downloader(context, Integer.MAX_VALUE)).build();
+        if (BuildConfig.DEBUG) {
+            picasso.setIndicatorsEnabled(true);
+        }
     }
 
     public void setGames(List<Game> games) {
@@ -40,8 +49,11 @@ public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.GameVi
 
     @Override
     public GameViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View cardView = LayoutInflater.from(parent.getContext()).inflate(R.layout.game_list_card, parent, false);
-        return new GameViewHolder(cardView);
+        return new GameViewHolder(inflateView(R.layout.game_list_card, parent, false));
+    }
+
+    View inflateView(int viewId, ViewGroup parent, boolean attachToParent) {
+        return LayoutInflater.from(parent.getContext()).inflate(viewId, parent, attachToParent);
     }
 
     @Override
@@ -52,7 +64,11 @@ public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.GameVi
 
     @Override
     public int getItemCount() {
-        return games == null ? 0 : games.size();
+        return isListEmpty() ? 0 : games.size();
+    }
+
+    boolean isListEmpty() {
+        return games == null || games.size() == 0;
     }
 
     public class GameViewHolder extends RecyclerView.ViewHolder {
@@ -63,12 +79,15 @@ public class GameListAdapter extends RecyclerView.Adapter<GameListAdapter.GameVi
 
         public GameViewHolder(View itemView) {
             super(itemView);
+            bindViews(itemView);
+        }
+
+        private void bindViews(View itemView) {
             ButterKnife.bind(this, itemView);
         }
 
         public void populate(GameData gameData) {
             if (gameData.boxImages.large != null && !gameData.boxImages.large.isEmpty()) {
-                picasso.setIndicatorsEnabled(true);
                 picasso.load(gameData.boxImages.large)
                         .placeholder(R.drawable.camera_loading)
                         .into(cover);
